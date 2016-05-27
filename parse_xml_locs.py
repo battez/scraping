519353897a5e9cbc48a9660870595ac0cfccbe6c
luke barker <j.l.p.barker@dundee.ac.uk>
2016-05-27 22:52:51 +0100

    adjust small things scrape

--- parse_xml_locs.py
+++ parse_xml_locs.py
@@ -15,9 +15,9 @@ import config
 
 # configuration
 xml_file = 'rssriver.xml' # this can be found from the RSS proxy url on SEPA site
-# conn  = 'localhost:27017'
+conn  = 'localhost:27017'
 conn_remote = config.MONGO_URI
-conn = conn_remote
+# conn = conn_remote
 database = 'Twitter'
 collection = 'gauges'
 
@@ -41,47 +41,69 @@ def scrape_avg_level(url):
         return 0
 
 
+if __name__ == '__main__':
+    ''' run the script, be sure to adjust the query as needed'''
 
-# set XML root tag
-e = ET.parse(xml_file).getroot()
+    MODE = 'bounding_box'
 
-# connect to db where we store the Gauge data:
-from pymongo import MongoClient
-client = MongoClient(conn)
-db = client[database]
+    # connect to db where we store the Gauge data:
+    from pymongo import MongoClient
+    client = MongoClient(conn)
+    db = client[database]
 
-# Loop through and make a document for each <item> to insert to DB.
-# elements = ['guid', 'description', 'link', 'georss:point']
-for count, item in enumerate(e.iterfind('channel/item')):
-    doc = {}
-    for idx, elem in enumerate(list(item.iter())[1:]):
-
-        # we want to grab the ID for ease of use later on, so do that:
-        if idx == 1:
-            doc['loc_id'] = (elem.text.rsplit('/', 1)[-1])
-        
-        # make a location with lat and lng, for GeoJSON tricks of mongodb
-        # note check ensureIndex() is set to 2dsphere
-        if idx == 4:
-            coords = [float(num) for num in elem.text.split(' ')]
-            
-            # x is long and y is lat, mongo prefers this ordering
-            doc['loc'] = { 'type' : 'Point', \
-            'coordinates' : [coords[1] , coords[0]] }
-          
-        else: 
-            doc[elem.tag] = elem.text
-
-    db[collection].insert(doc)
+    if(MODE == 'populate'):
+        # set XML root tag
+        e = ET.parse(xml_file).getroot()
 
 
+        # Loop through and make a document for each <item> to insert to DB.
+        # elements = ['guid', 'description', 'link', 'georss:point']
+        for count, item in enumerate(e.iterfind('channel/item')):
+            doc = {}
+            for idx, elem in enumerate(list(item.iter())[1:]):
 
-# loop through all the db collection of gauges
-docs = db[collection].find()
-for doc in docs:
+                # we want to grab the ID for ease of use later on, so do that:
+                if idx == 1:
+                    doc['loc_id'] = (elem.text.rsplit('/', 1)[-1])
+                
+                # make a location with lat and lng, for GeoJSON tricks of mongodb
+                # note check ensureIndex() is set to 2dsphere
+                if idx == 4:
+                    coords = [float(num) for num in elem.text.split(' ')]
+                    
+                    # x is long and y is lat, mongo prefers this ordering
+                    doc['loc'] = { 'type' : 'Point', \
+                    'coordinates' : [coords[1] , coords[0]] }
+                  
+                else: 
+                    doc[elem.tag] = elem.text
 
-    # retrieve avg level
-    val = scrape_avg_level(doc['link'])
-   
-    # update this record with this new attribute 
-    db[collection].update({'loc_id': doc['loc_id']}, {'$set': {'avg_level':val}})
+            db[collection].insert(doc)
+
+
+    else:
+
+        # loop through all the db collection of gauges
+        docs = db[collection].find()
+        for doc in docs:
+
+            if(MODE == 'level'):
+
+                # retrieve avg level
+                val = scrape_avg_level(doc['link'])
+               
+                # update this record with this new attribute 
+                db[collection].update({'loc_id': doc['loc_id']}, {'$set': {'avg_level':val}})
+
+            else:
+                import bounding_box
+                # retrieve bounding box
+                val = bounding_box.get_bounding_box( doc['loc']['coordinates'][1], \
+                                        doc['loc']['coordinates'][0], \
+                                        5  )
+               
+                # update this record with this new attribute 
+                
+                db[collection].update({'loc_id': doc['loc_id']}, {'$set': {'bounding_box':[ \
+                    val.lon_min, val.lat_min, val.lon_max, val.lat_max]}})
+               
